08480ea7d398f08fe32a9cbf9c30ef937391150f
shehantissera <shehan@duosoftware.com>
2017-02-06 15:54:04 +0530

    latest changes been done

--- js/app.js
+++ js/app.js
@@ -18,6 +18,30 @@ angular.module('smoothflowwebsite', [
         }).when('/howitworks', {
             templateUrl: 'pages/howitworks.html',
             controller: 'mainController'
+        }).when('/client', {
+            templateUrl: 'pages/client.html',
+            controller: 'mainController'
+        }).when('/activities', {
+            templateUrl: 'pages/activities.html',
+            controller: 'mainController'
+        }).when('/blog', {
+            templateUrl: 'pages/blog.html',
+            controller: 'mainController'
+        }).when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'mainController'
+        }).when('/faq', {
+            templateUrl: 'pages/faq.html',
+            controller: 'mainController'
+        }).when('/acceptable-use-policy', {
+            templateUrl: 'pages/acceptable-use-policy.html',
+            controller: 'mainController'
+        }).when('/terms-and-conditions', {
+            templateUrl: 'pages/terms-and-conditions.html',
+            controller: 'mainController'
+        }).when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'mainController'
         }).otherwise({ redirectTo: '/pagenotfound' });
     }])
 
